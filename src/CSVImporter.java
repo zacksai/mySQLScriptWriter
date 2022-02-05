b3407d57f08d9bb0b50fa1a01b0bc3388a1056d1
zacksai <zackjsai@gmail.com>
2022-02-05 00:14:42 -0800

    added changed files

--- src/CSVImporter.java
+++ src/CSVImporter.java
@@ -1,26 +1,30 @@
-import java.io.File;
+import java.io.BufferedReader;
 import java.io.FileNotFoundException;
+import java.io.FileReader;
 import java.util.Scanner;
 import java.util.Stack;
 
 /**
  * CSV Importer creates mySQL table import statements using the first line of given CSVs
- * <p>
- * Step 1: add all the file paths to a stack;
- * Step 2: iterate through each file path
- * Step 2a: read in first line of each file
- * Step 2b: create a table import statement out of each line
- * Step 2c: store it into a txt file
- * Later considerations: drop empty columns
  */
 public class CSVImporter {
+
+    /*
+     * Step 1: add all the file paths to a stack;
+     * Step 1a: prompt for input using loop
+     * Step 1b: Add input to stack until user exits
+     * Step 2: iterate through each file path
+     * Step 2a: read in first line of each file
+     * Step 2b: create a table import statement out of each line
+     * Step 2c: store it into a txt file
+     * Later considerations: drop empty columns
+     */
+
 
     public static void main(String[] args) {
 
 
-        // Step 1: add all the file paths to a stack;
-            // Step 1a: prompt for input using loop
-            // Step 1b: Add input to stack until user exits
+
 
 
 
@@ -35,25 +39,29 @@ public class CSVImporter {
         Scanner keys = new Scanner(System.in);
         Stack<String> fileNames = new Stack<>();
 
-
-
-        Scanner testFile = null;
-        try {
-            testFile = new Scanner(new File("/Users/zacksai/Documents/WIT/SQLCreator/WIT/src/all_downloaded_table_record_counts.csv"));
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
-        }
+        promptForInput(keys, fileNames);
+        readEachFile(fileNames);
 
 
     }
 
-    private static void promptForFileName(Scanner inputScanner, Stack fileNameStack) {
+    private static void readEachFile(Stack<String> fileNames) {
 
-        // Prompt for file name and add the file to the stack
-        System.out.println("Enter file name:");
+        // Create strings to contain line read and output script
+        String lineRead = "", outputScript = "";
 
+        // Iterate through file names
+        while (!fileNames.empty()){
 
-        fileNameStack.push(inputScanner.next());
+            // Create a fileReader and create a script string
+            try {
+                FileReader csvReader = new FileReader(fileNames.pop());
+            } catch (FileNotFoundException e) {
+                System.out.println("Error: This file isn't in the directory: " + fileNames.pop());
+                e.printStackTrace();
+            }
+        }
+
     }
 
     /**
@@ -61,20 +69,25 @@ public class CSVImporter {
      *
      * @param consoleInput reads input from scanner
      */
-    public static void promptForInput(Scanner consoleInput, Stack fileNames) {
+    public static void promptForInput(Scanner consoleInput, Stack<String> fileNames) {
 
-        // Create file variable:
+        // Welcome statement
+        System.out.println("Welcome to CSV Importer!");
+
+        // Create string to store input
         String nextFilePasted = "";
 
-        // Prompt for input
-        System.out.println("Paste the next CSV here, or type \"exit\" to exit.");
+        // Continue loop until prompted to exit
+        while (!nextFilePasted.equals("exit")) {
 
-
-        // Exit when prompted
-        if (consoleInput.next() != "1") {
-            System.out.println("Thank you! Your SQL script is complete.");
-            System.exit(0);
+            // Prompt for input and add it to the stack
+            System.out.println("Paste the next CSV here, or type \"exit\" to exit.");
+            nextFilePasted = consoleInput.nextLine();
+            // Test message System.out.println("NextFilePasted = " + nextFilePasted);
+            fileNames.push(nextFilePasted);
         }
+
+        System.out.println("Thanks for your input. Creating your script...");
 
     }
 }
